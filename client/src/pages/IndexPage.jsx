3db0d7a8c0e53f51d6e599111af0f8061c0c3f05
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-05-14 23:32:26 +0200

    login page

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 function IndexPage() {
   return (
-    <div>
-        Index page here
+    <div className='max-w-[1440px]'>
+        <div className='w-full'>
+            
+
+        </div>
     </div>
   )
 }
