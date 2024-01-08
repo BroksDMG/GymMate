41f9e497d22b365008e8a17c951eafdd2647e80f
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-01-08 23:02:20 +0100

    for deploy on netlify

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: process.env.CLIENT_URL,
+    origin: "https://main--poetic-duckanoo-7c796e.netlify.app",
   })
 );
 console.log(process.env.MONGO_URL);
