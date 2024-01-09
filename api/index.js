2aa154438063346daae7579bb76f24101f7d8f46
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-01-09 15:57:23 +0100

    changed env for heroku deploy

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "https://poetic-duckanoo-7c796e.netlify.app",
+    origin: process.env.CLIENT_URL,
   })
 );
 console.log(process.env.MONGO_URL);
