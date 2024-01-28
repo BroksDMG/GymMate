0cf890a5fce66e4cf887c5fecf6228168522c0b8
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-01-28 23:46:16 +0100

    small css changes

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -6,37 +6,9 @@ const Modal = ({ isOpen, children, onClose }) => {
   }
 
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: "#fff",
-          padding: "20px",
-          borderRadius: "8px",
-          width: "80%",
-          maxWidth: "500px",
-        }}
-      >
-        <button
-          onClick={onClose}
-          style={{
-            float: "right",
-            border: "none",
-            backgroundColor: "transparent",
-            fontSize: "1.5em",
-          }}
-        >
+    <div className=" rounded flex fixed justify-center items-center w-full h-full">
+      <div className="bg-lightBrown text-black text-center text-2xl font-bold relative">
+        <button onClick={onClose} className="float-right">
           ×
         </button>
         {children}
