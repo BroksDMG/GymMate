4e060ed0630975d6812877c9147478ad6dab325f
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-02-09 23:04:52 +0100

    first steps for messege functionality

--- client/src/pages/MessagesPage.jsx
+++ client/src/pages/MessagesPage.jsx
@@ -1,13 +1,49 @@
-import error404Img from "../assets/404-Page-Featured-Image.png";
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
 
 function MessagesPage() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+  const socketRef = useRef();
+
+  useEffect(() => {
+    socketRef.current = io.connect("/"); // Connect to the server
+
+    socketRef.current.on("message", ({ message }) => {
+      setChat([...chat, message]);
+    });
+
+    return () => socketRef.current.disconnect();
+  }, [chat]);
+
+  const onTextChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    const msg = { message };
+    socketRef.current.emit("message", msg);
+    setMessage("");
+  };
+
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <img
-        src={error404Img}
-        alt="error404"
-        className="h-full w-full object-contain object-center "
-      />
+    <div>
+      <h1>Messenger</h1>
+      <form onSubmit={onMessageSubmit}>
+        <input
+          name="message"
+          onChange={(e) => onTextChange(e)}
+          value={message}
+          label="Message"
+        />
+        <button>Send Message</button>
+      </form>
+      <div>
+        {chat.map((msg, idx) => (
+          <div key={idx}>{msg}</div>
+        ))}
+      </div>
     </div>
   );
 }
