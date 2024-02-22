15eb0015eb2db2a9cefc6b091da5dc669409416c
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-02-22 09:54:00 +0100

    added getMessages and get UsersSideBar

--- api/controllers/messageController.js
+++ api/controllers/messageController.js
@@ -23,14 +23,46 @@ const sendMessage = async (req, res) => {
     if (newMessage) {
       conversation.messages.push(newMessage);
     }
-    await conversation.save();
-    await newMessage.save();
+    // await conversation.save();
+    // await newMessage.save();
+
+    await Promise.all([conversation.save(), newMessage.save()]);
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in senMessage controller:", error.message);
     res.status(500).json({ error: "internal server error" });
   }
 };
+const getMessage = async (req, res) => {
+  try {
+    const usertoChatId = req.params.id;
+    const senderId = req.user._id;
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, reciverId] },
+    }).populate("messages");
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+    res.status(200).json(conversation.messages);
+  } catch (error) {
+    console.log("Error in getMessage controller:", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
+const getUsersForSidebar = async (req, res) => {
+  try {
+    const LoggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: LoggedInUserId },
+    }).select("-password");
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getUsersForSidebar controller:", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
 module.exports = {
   sendMessage,
+  getMessage,
+  getUsersForSidebar,
 };
--- api/routes/messageRoute.js
+++ api/routes/messageRoute.js
@@ -4,4 +4,10 @@ const messageController = require("../controllers/messageController.js");
 const protectRoute = require("../middleware/protectRoute.js");
 
 router.post("/sendMessage/:id", protectRoute, messageController.sendMessage);
+router.get("/getMessage/:id", protectRoute, messageController.getMessage);
+router.get(
+  "/getUsersForSidebar",
+  protectRoute,
+  messageController.getUsersForSidebar
+);
 module.exports = router;
