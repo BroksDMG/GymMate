1642b24dc28af54b92ad732de9a4ae092a45f9ed
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-03-11 10:22:37 +0100

    searching conversation done

--- client/src/components/Messages/SideBar.jsx
+++ client/src/components/Messages/SideBar.jsx
@@ -1,15 +1,42 @@
 import InputField from "../InputField";
+import useGetConversation from "../hooks/useGetConversation";
+import { useState } from "react";
 import SideBarConversation from "./SideBarConversations";
 import propTypes from "prop-types";
-function SideBar({ chat, chatId, setChatId, setChatAvatars }) {
+function SideBar({ setChatId, setChatAvatars }) {
+  const { loading, conversation } = useGetConversation();
+  const [searchConversation, setSearchConversation] = useState("");
+  const [searchedConversation, setSearchedConversation] = useState([]);
+  const handleSearch = (searchValue) => {
+    setSearchConversation(searchValue);
+    if (searchValue.length < 3) {
+      setSearchedConversation([]);
+      return;
+    }
+    const filteredConversation = conversation?.filter((event) => {
+      const nameCondition = event.name
+        .toLowerCase()
+        .includes(searchValue.toLowerCase());
+
+      const surnameCondition = event.surname
+        .toLowerCase()
+        .includes(searchValue.toLowerCase());
+      return nameCondition || surnameCondition;
+    });
+    setSearchedConversation(filteredConversation);
+  };
   return (
     <div className="h-full">
-      <InputField></InputField>
+      <InputField
+        value={searchConversation}
+        onChange={(e) => handleSearch(e.target.value)}
+      ></InputField>
       <SideBarConversation
-        chat={chat}
-        chatId={chatId}
         setChatId={setChatId}
+        searchedConversation={searchedConversation}
         setChatAvatars={setChatAvatars}
+        conversation={conversation}
+        loading={loading}
       />
     </div>
   );
@@ -18,7 +45,5 @@ function SideBar({ chat, chatId, setChatId, setChatAvatars }) {
 export default SideBar;
 SideBar.propTypes = {
   setChatAvatars: propTypes.func,
-  chat: propTypes.array,
-  chatId: propTypes.string,
   setChatId: propTypes.func,
 };
--- client/src/components/Messages/SideBarConversations.jsx
+++ client/src/components/Messages/SideBarConversations.jsx
@@ -1,19 +1,31 @@
-import useGetConversation from "../hooks/useGetConversation";
 import Conversation from "./Conversation";
 import propTypes from "prop-types";
-function SideBarConversations({ chat, chatId, setChatId, setChatAvatars }) {
-  const { loading, conversation } = useGetConversation();
-
+function SideBarConversations({
+  setChatId,
+  setChatAvatars,
+  conversation,
+  loading,
+  searchedConversation,
+}) {
   return (
     <div className="pt-2 flex flex-col overflow-auto">
-      {conversation.map((conversation) => (
-        <Conversation
-          key={conversation._id}
-          conversation={conversation}
-          setChatId={setChatId}
-          setChatAvatars={setChatAvatars}
-        />
-      ))}
+      {searchedConversation.length > 0
+        ? searchedConversation.map((conversation) => (
+            <Conversation
+              key={conversation._id}
+              conversation={conversation}
+              setChatId={setChatId}
+              setChatAvatars={setChatAvatars}
+            />
+          ))
+        : conversation.map((conversation) => (
+            <Conversation
+              key={conversation._id}
+              conversation={conversation}
+              setChatId={setChatId}
+              setChatAvatars={setChatAvatars}
+            />
+          ))}
 
       {loading && (
         <span className="loading loading-spinner mx-auto">Loading...</span>
@@ -25,7 +37,8 @@ function SideBarConversations({ chat, chatId, setChatId, setChatAvatars }) {
 export default SideBarConversations;
 SideBarConversations.propTypes = {
   setChatAvatars: propTypes.func,
-  chat: propTypes.array,
-  chatId: propTypes.string,
   setChatId: propTypes.func,
+  conversation: propTypes.array,
+  loading: propTypes.bool,
+  searchedConversation: propTypes.array,
 };
