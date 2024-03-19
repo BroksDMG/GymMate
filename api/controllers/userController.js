d000ef4de0b5c352a7bcb57c175c716b82f2c703
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-03-19 21:03:36 +0100

    chenges with fiters

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Event = require("../models/Event");
 const bcrypt = require("bcrypt");
 const generateToken = require("../middleware/generateToken");
 const mongoose = require("mongoose");
